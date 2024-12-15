e0c9354414bee6569e8fc6065d70acc087811beb
Le Hai Anh <haianh102003@gmail.com>
2024-12-15 22:14:39 +0700

    Init Header

--- src/app/controrllers/siteController.js
+++ src/app/controrllers/siteController.js
@@ -1,7 +1,7 @@
 class SiteController {
-    index(req, res) {
-        res.render('home');
-      }
+  index(req, res) {
+    res.render('home');
+  }
 }
 
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ const router = require('./router/index');
 const app = express();
 const port = 4000;
 
-
 app.use(express.static(path.join(__dirname, 'resources', 'public')));
 
-
-app.engine('hbs', hbs.engine({
-    extname: '.hbs'
-}));
+app.engine(
+  'hbs',
+  hbs.engine({
+    extname: '.hbs',
+  }),
+);
 app.set('views', path.join(__dirname, 'resources', 'views'));
 app.set('view engine', 'hbs');
 
 router(app);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`),
+);
